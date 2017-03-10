2a5346dec230ab72204cad5faa5e0bfb5c6d95c1
tomatronic <tom.m.spencer@gmail.com>
2017-03-10 15:50:23 +0000

    Update boot.js

--- boot.js
+++ boot.js
@@ -7,7 +7,7 @@ var $dd = $yesterday.getDate();
 var $mm = $yesterday.getMonth()+1; //January is 0!
 
 var $yyyy = $yesterday.getFullYear();
-if($dd<10){$dd='0'+dd} if($mm<10){$mm='0'+$mm} $yesterday = $dd+'/'+$mm+'/'+$yyyy;
+if($dd<10){$dd='0'+$dd} if($mm<10){$mm='0'+$mm} $yesterday = $dd+'/'+$mm+'/'+$yyyy;
 
 console.log($yesterday);
 
