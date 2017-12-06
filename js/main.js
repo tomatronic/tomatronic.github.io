5c6139f6248605bc6b6cef4007779e92b11ba859
tomatronic <tom.m.spencer@gmail.com>
2017-12-06 12:55:14 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
-$(document).ready(function(){
-  var scrollTop = 0;
+$(document).ready(function() {
+
+//Mobile navigation
+  $('.menu-toggle').click(function() {
+      if ($('.navbar').hasClass('open-nav')) {
+          $('.navbar').removeClass('open-nav');
+      } else {
+          $('.navbar').addClass('open-nav');
+      }
+  });
+
+  	$('a[href^="#"]').on('click',function (e) {
+  	    e.preventDefault();
+
+  	    var target = this.hash;
+  	    var $target = $(target);
+
+  	    $('html, body').stop().animate({
+  	        'scrollTop': $target.offset().top - 120
+  	    }, 900, 'swing');
+  	});
+
+
+//function for arrow fade out
   $(window).scroll(function(){
-    scrollTop = $(window).scrollTop();
-     $('.counter').html(scrollTop);
-    
-    if (scrollTop >= 100) {
-      $('#globalNav').addClass('scrolled-nav');
-    } else if (scrollTop < 100) {
-      $('#globalNav').removeClass('scrolled-nav');
-    } 
-    
-  }); 
-  
+      $(".scroll").css("opacity", 1 - $(window).scrollTop() / 250);
+    //250 is fade pixels
+    });
+
+
 });
